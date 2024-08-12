fd691a8ace564a1ecd107040d553100011a1bc91
qeta fetviashvili <qetafetviashvili123@gmail.com>
2024-08-12 15:18:40 +0400

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This will generate the `dist` folder inside the root of `vite-project`
+  },
 })
